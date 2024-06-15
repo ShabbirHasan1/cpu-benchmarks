241076bcea1403b29945ae82cb96ad84cefb0325
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-06-15 17:58:25 +0200

    add readme with some links

--- plot.py
+++ plot.py
@@ -12,25 +12,39 @@ data["sz"] = data["size"]
 
 data = data[data.sz >= l1_size // 4]
 
-# Create a figure
-fig, ax = plt.subplots()
-ax.set_title("Pointer Chasing")
-ax.set_xlabel("Array Size (bytes)")
-ax.set_ylabel("Latency (ns)")
-# Plot a single line for each experiment.
-for name in data.name.unique():
-    ax.plot("sz", "latency", data=data[data.name == name], label=name)
-ax.set_xscale("log", base=2)
-ax.set_ylim(0, None)
-ax.yaxis.grid(True)
-ax.legend()
 
-# Add vertical lines for cache sizes
-ax.axvline(x=l1_size, color="red", linestyle="--")
-ax.axvline(x=l2_size, color="red", linestyle="--")
-if l3_size <= data.sz.max():
-    ax.axvline(x=l3_size, color="red", linestyle="--")
+def plot(data, fig_name):
+    # Create a figure
+    fig, ax = plt.subplots()
+    ax.set_title("Pointer Chasing")
+    ax.set_xlabel("Array Size (bytes)")
+    ax.set_ylabel("Latency (NS)")
+    # Plot a single line for each experiment.
+    for name in data.name.unique():
+        ax.plot(
+            "sz",
+            "latency",
+            data=data[data.name == name],
+            label=name,
+            linestyle="--",
+            linewidth=1,
+        )
+    ax.set_xscale("log", base=2)
+    ax.set_ylim(0, None)
+    ax.yaxis.grid(True)
+    # legend on the right
+    ax.legend(loc="upper right", bbox_to_anchor=(1, 1))
 
-# Save
-fig.savefig("plots/pointer-chasing.png")
-fig.savefig("plots/pointer-chasing.svg")
+    # Add vertical lines for cache sizes
+    ax.axvline(x=l1_size, color="red", linestyle="--")
+    ax.axvline(x=l2_size, color="red", linestyle="--")
+    if l3_size <= 2 * data.sz.max():
+        ax.axvline(x=l3_size, color="red", linestyle="--")
+
+    # Save
+    fig.savefig(f"plots/{fig_name}.png", bbox_inches="tight", dpi=600)
+    fig.savefig(f"plots/{fig_name}.svg", bbox_inches="tight")
+
+
+plot(data[~data.name.str.contains("work")], "pointer-chasing")
+plot(data[data.name.str.contains("work")], "pointer-chasing-work")
